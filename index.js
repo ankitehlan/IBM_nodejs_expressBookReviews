507aae8fc7e2a85330d3f7f5f58b2bedf940699a
Ankit Tehlan <ankitehlan@gmail.com>
2023-10-06 19:10:53 +0530

    Add implementation for registering user

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express, { json } from 'express';
 import jwt from 'jsonwebtoken';
 import session from 'express-session';
 import { authenticated as customer_routes } from './router/auth_users.js';
-import { general as genl_routes } from './router/general.js';
+import { general as general_routes } from './router/general.js';
 
 const app = express();
 
 app.use(json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:"jwtsecret",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
@@ -17,6 +17,6 @@ app.use("/customer/auth/*", function auth(req,res,next){
 const PORT = 5001;
 
 app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+app.use("/", general_routes);
 
 app.listen(PORT,()=>console.log(`Server is running on ${PORT}`));
--- router/auth_users.js
+++ router/auth_users.js
@@ -5,9 +5,16 @@ const regd_users = Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  let user = users.filter((user) => {
+    return user.username === username;
+  });
+  if (user.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
@@ -19,12 +26,12 @@ regd_users.post("/login", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Add a book revie
+// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 export const authenticated = regd_users;
-export const _isValid = isValid;
-export const _users = users;
+export const doesExist = isValid;
+export const registeredUsers = users;
