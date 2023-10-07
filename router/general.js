175e12abef3e62fecea00047c9491107a04f05c8
Ankit Tehlan <ankitehlan@gmail.com>
2023-10-07 21:14:30 +0530

    Use promises to get books with filters

--- router/general.js
+++ router/general.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, response } from "express";
 import books from "./booksdb.js";
 import { doesExist } from "./auth_users.js";
 import { registeredUsers } from "./auth_users.js";
@@ -21,42 +21,73 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  res.send(books);
+  let myPromise = new Promise((resolve, reject) => {
+    resolve(books);
+  });
+  myPromise.then((response) => {
+    res.send(response);
+  });
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  let filteredBooks = books[isbn];
-  res.send(filteredBooks);
+  let myPromise = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    let filteredBooks = books[isbn];
+    resolve(filteredBooks);
+  });
+
+  myPromise.then((response) => {
+    res.send(response);
+  });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  const author = req.params.author;
-  let filteredBooks = Object.entries(books).filter((book) => {
-    let bookAuthor = book[1]["author"];
-    return bookAuthor.includes(author);
+  let myPromise = new Promise((resolve, reject) => {
+    const author = req.params.author;
+    let filteredBooks = Object.entries(books).filter((book) => {
+      let bookAuthor = book[1]["author"];
+      return bookAuthor.includes(author);
+    });
+    if (filteredBooks.length > 0) {
+      resolve(filteredBooks);
+    } else {
+      reject(`No books found from author: ${author}`);
+    }
   });
-  if (filteredBooks.length > 0) {
-    res.send(filteredBooks);
-  } else {
-    res.send(`No books found from author: ${author}`);
-  }
+
+  myPromise
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  const title = req.params.title;
-  let filteredBooks = Object.entries(books).filter((book) => {
-    let bookTitle = book[1]["title"];
-    return bookTitle.includes(title);
+  let myPromise = new Promise((resolve, reject) => {
+    const title = req.params.title;
+    let filteredBooks = Object.entries(books).filter((book) => {
+      let bookTitle = book[1]["title"];
+      return bookTitle.includes(title);
+    });
+    if (filteredBooks.length > 0) {
+      resolve(filteredBooks);
+    } else {
+      reject(`No books found with title: ${title}`);
+    }
   });
-  if (filteredBooks.length > 0) {
-    res.send(filteredBooks);
-  } else {
-    res.send(`No books found with title: ${title}`);
-  }
+
+  myPromise
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 //  Get book review
